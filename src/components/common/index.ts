071c36a0c9ed1a1ff554e04a418ced0939466a0f
Israfil Guseinov <israfil.guseynov.98@gmail.com>
2026-03-05 15:48:11 -0800

    feat: export icon components from common barrel file

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button';
 export { default as ErrorBoundary } from './ErrorBoundary';
+export { GitHubIcon, GoogleIcon } from './Icons';
 export { default as SectionHeader } from './SectionHeader';
